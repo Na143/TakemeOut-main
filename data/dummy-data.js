ecec7ca4adc6dc60a1a83d082f7b13228a60a9fe
Djole541 <74978975+Djole541@users.noreply.github.com>
2022-02-23 18:14:41 +0100

    add event

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -1,5 +1,5 @@
 import Category from '../models/category';
-import ToDoItem from '../models/todoitem';
+import Event from '../models/event';
 
 export const CATEGORIES = [
   new Category('c1', 'Restaurants', '#f5428d'),
@@ -10,29 +10,28 @@ export const CATEGORIES = [
   new Category('c6', 'Konzerte', '#41d95d'),
   new Category('c7', 'Clubs', '#9eecff'),
   new Category('c8', 'Family & Kids', '#b9ffb0'),
-  new Category('c9', '...'),
-  new Category('c10', 'Things to Learn', '#47fced')
+  
 ];
 
-export const TODOITEMS = [
-  new ToDoItem('m1', 'c1', 'Finish Project Maven', true),
-  new ToDoItem('m2', 'c1', 'Call Ellie', false),
-  new ToDoItem('m3', 'c1', 'Take 15 min break', false),
-  new ToDoItem('m4', 'c1', 'Research React Native', true),
-  new ToDoItem('m5', 'c2', 'Finish homework assignment 1', false),
-  new ToDoItem('m6', 'c2', 'Get new Student ID Card', false),
-  new ToDoItem('m7', 'c3', 'Buy Tomatoes', false),
-  new ToDoItem('m8', 'c3', 'Buy Beer', true),
-  new ToDoItem('m9', 'c3', 'Buy Milk', true),
-  new ToDoItem('m10', 'c4', 'Finish painting the boat', false),
-  new ToDoItem('m11', 'c5', 'Call mom', false),
-  new ToDoItem('m12', 'c5', 'Give back money to sis', false),
-  new ToDoItem('m13', 'c6', 'Raspberry Pi', false),
-  new ToDoItem('m14', 'c6', 'Socks', false),
-  new ToDoItem('m15', 'c6', 'Cookbook', true),
-  new ToDoItem('m16', 'c6', 'Yoga mat', true),
-  new ToDoItem('m17', 'c7', 'Fix the AC', false),
-  new ToDoItem('m18', 'c8', 'Bring Sunglasses', false),
-  new ToDoItem('m19', 'c9', 'Write a new song', false),
-  new ToDoItem('m20', 'c10', 'Learn React Native', true)
+export const EVENTS = [
+  new Event('m1', 'c1', 'El Toro','Kleiner Schäferkamp 21, 20357 Hamburg', true),
+  new Event('m2', 'c1', 'Otto\'s Burger','Schanzenstraße 58,20357 Hamburg', false),
+  new Event('m3', 'c1', 'Kimo','Schanzenstraße 111, 20357 Hamburg', false),
+  new Event('m4', 'c1', 'Yin Yang','Lattenkamp 8, 22299 Hamburg', true)
+  // new Event('m5', 'c2', 'Finish homework assignment 1', false),
+  // new Event('m6', 'c2', 'Get new Student ID Card', false),
+  // new Event('m7', 'c3', 'Buy Tomatoes', false),
+  // new Event('m8', 'c3', 'Buy Beer', true),
+  // new Event('m9', 'c3', 'Buy Milk', true),
+  // new Event('m10', 'c4', 'Finish painting the boat', false),
+  // new Event('m11', 'c5', 'Call mom', false),
+  // new Event('m12', 'c5', 'Give back money to sis', false),
+  // new Event('m13', 'c6', 'Raspberry Pi', false),
+  // new Event('m14', 'c6', 'Socks', false),
+  // new Event('m15', 'c6', 'Cookbook', true),
+  // new Event('m16', 'c6', 'Yoga mat', true),
+  // new Event('m17', 'c7', 'Fix the AC', false),
+  // new Event('m18', 'c8', 'Bring Sunglasses', false),
+  // new Event('m19', 'c9', 'Write a new song', false),
+  // new Event('m20', 'c10', 'Learn React Native', true)
 ];
